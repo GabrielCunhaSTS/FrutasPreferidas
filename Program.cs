8a2aa9738c90ace0e96af2e491f11d60e263963d
Gabriel <ggabrielcunha06@gmail.com>
2025-02-24 11:05:55 -0300

    finaliza programa que exibe frutas favoritas

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("*--------------------------------*");
+Console.WriteLine("|        Frutas Favoritas        |");
+Console.WriteLine("*--------------------------------* \n");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("1. Uva");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("2. Melancia");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("3. Pera");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("4. Maçã");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("5. Morango");
+Console.ResetColor();
